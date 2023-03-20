071655528b1a4c9f16cc67b5ff22b479123d2529
Reagan Armstrong <rarmst0@gmail.com>
2023-03-20 16:14:33 -0400

    Renaming server file from galiga to app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const galiga = express();
+const app = express();
 
-galiga.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-galiga.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
