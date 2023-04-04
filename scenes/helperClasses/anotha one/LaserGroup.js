fcca8297d862ef06f10bf116f3bc950611f5bfeb
Nicholas Marthinuss <nmarth2993@gmail.com>
2023-04-03 21:39:42 -0400

    Revert "Revert "Sdenonc sprint2 demo""

--- scenes/helperClasses/anotha one/LaserGroup.js
+++ scenes/helperClasses/anotha one/LaserGroup.js
@@ -10,17 +10,17 @@ class LaserGroup extends Phaser.Physics.Arcade.Group {
       classType: Laser, // This is the class we create just below
       // the below value constrains the maximum number of objects on screen at once
       // this determines how many bullets the player can fire before having to wait
-      frameQuantity: 4,
+      frameQuantity: 3,
       active: false,
       visible: false,
       key: "missile",
     });
   }
 
-  fireLaser(x, y, xVel, yVel) {
+  fireLaser(x, y) {
     const laser = this.getFirstDead(false);
     if (laser) {
-      laser.fire(x, y, xVel, yVel);
+      laser.fire(x, y);
     }
   }
 }
--- scenes/testScene.js
+++ scenes/testScene.js
@@ -7,65 +7,77 @@ class testScene extends Scene {
     // must load from webserver (even if localhost) instead of from a local file
     // due to security reasons
     this.load.image("ship", "assets/png/ship.png");
+    this.load.image("ship_left", "assets/png/ship_left.png");
+    this.load.image("ship_right", "assets/png/ship_right.png");
     this.load.image("missile", "assets/png/missile.png");
-    this.load.setCORS("Access-Control-Allow-Origin: http://labs.phaser.io");
-    this.load.image("sky", "http://labs.phaser.io/assets/skies/space3.png");
-    this.load.image(
-      "logo",
-      "http://labs.phaser.io/assets/sprites/phaser3-logo.png"
-    );
-    this.load.image("red", "http://labs.phaser.io/assets/particles/red.png");
+    this.load.image("sky", "assets/png/sky.png");
   }
 
   create() {
-    this.add.image(400, 300, "sky");
-
+    this.add.image(512, 384, "sky");
     this.laserGroup = new LaserGroup(this);
-
     this.keys = this.input.keyboard.addKeys("LEFT,RIGHT,UP,DOWN,SPACE");
+    this.ship = this.physics.add.image(512, 700, "ship");
+    //this.ship.setCollideWorldBounds(true);
 
-    let particles = this.add.particles("red");
-
-    let emitter = particles.createEmitter({
-      speed: 20,
-      scale: { start: 0.5, end: 0 },
-      lifespan: 400,
-      blendMode: "ADD",
-    });
-
-    this.ship = this.physics.add.image(300, 700, "ship");
-    this.ship.setCollideWorldBounds(true);
-
-    // var logo = this.physics.add.image(400, 100, 'logo');
-
-    // logo.setVelocity(100, 200);
-    // logo.setBounce(1, 1);
-    // logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(this.ship);
+    this.debugText = this.add.text(16, 16, 'hello');
   }
 
   update() {
+    this.physics.world.wrap(this.ship);
+
+    this.debugText.setText("fps: " + this.game.loop.actualFps.toString() + "\n"
+			   + "y: " + this.ship.y + "\n"
+			   + "y velocity: " + this.ship.body.velocity.y.toString() + "\n"
+			   + "x velocity: " + this.ship.body.velocity.x.toString() + "\n");   
+
+    if (this.ship.y >= 734 && this.ship.body.velocity.y > 0) {
+      this.ship.body.velocity.y = 0;
+    }
     if (this.keys.UP.isDown) {
       console.log("up");
-      this.ship.setPosition(this.ship.x, this.ship.y - 2);
-    } else if (this.keys.DOWN.isDown) {
-      console.log("down");
-      this.ship.setPosition(this.ship.x, this.ship.y + 2);
-    }
-    if (this.keys.LEFT.isDown) {
-      console.log("left");
-      this.ship.setPosition(this.ship.x - 2, this.ship.y);
-    } else if (this.keys.RIGHT.isDown) {
-      console.log("right");
-      this.ship.setPosition(this.ship.x + 2, this.ship.y);
-    }
+      //this.ship.setVelocityY(this.ship.body.velocity.y - 4);
+      this.ship.setVelocityY(500 - this.ship.y);
 
-    // if (Phaser.Input.Keyboard.JustDown)
+    }
+    if (this.keys.DOWN.isDown) {
+      console.log("down");
+      /*if (this.ship.y < 734) {
+        this.ship.setVelocityY(this.ship.body.velocity.y + 4);
+      }*/
+      this.ship.setVelocityY(734 - this.ship.y);      
+    }
+    if (this.keys.LEFT.isDown && !this.keys.RIGHT.isDown) {
+      console.log("left");
+      if (this.ship.body.velocity.x > 400) {
+        this.ship.setTexture('ship')
+      }
+      else {
+        this.ship.setTexture('ship_left')
+      }
+      this.ship.setVelocityX(this.ship.body.velocity.x - 10)
+    }
+    if (this.keys.RIGHT.isDown && !this.keys.LEFT.isDown) {
+      console.log("right");
+      if (this.ship.body.velocity.x < -400) {
+        this.ship.setTexture('ship')
+      }
+      else {
+        this.ship.setTexture('ship_right')
+      }
+      this.ship.setVelocityX(this.ship.body.velocity.x + 10);
+    }
+    if (!this.keys.RIGHT.isDown && !this.keys.LEFT.isDown){
+      this.ship.setTexture('ship');
+      this.ship.setVelocityX(this.ship.body.velocity.x * 0.98);
+    }
+    if (!this.keys.UP.isDown && !this.keys.DOWN.isDown){
+      this.ship.setVelocityY(this.ship.body.velocity.y * 0.98);
+    }
 
     if (Phaser.Input.Keyboard.JustDown(this.keys.SPACE)) {
       console.log("shooted");
-      this.laserGroup.fireLaser(this.ship.x, this.ship.y - 20);
+      this.laserGroup.fireLaser(this.ship.x + (this.ship.body.velocity.x * .03), this.ship.y - 48, this.ship.body.velocity.x, this.ship.body.velocity.y);
     }
   }
 }
