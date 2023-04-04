6f8982981c4b779cbe4463d6d778406dbe985c58
Reagan Armstrong <rarmst0@gmail.com>
2023-04-03 20:08:14 -0400

    Unzipped folder

---
+++ scenes/helperClasses/Laser.js
@@ -0,0 +1,31 @@
+import Phaser from "phaser";
+class Laser extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, "missile");
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+    if (this.y <= 0) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  }
+
+  fire(x, y, xVel, yVel) {
+    this.body.reset(x, y);
+    this.setActive(true);
+    this.setVisible(true);
+
+    //if the ship doesn't have upwards speed greater
+    //than the minimum bullet speed
+    if (yVel >= -700){
+      this.setVelocity(xVel, -700);
+    }
+    else{
+      this.setVelocity(xVel, yVel - 100);
+    }
+  }
+
+}
+export default Laser;
---
+++ scenes/helperClasses/LaserGroup.js
@@ -0,0 +1,27 @@
+import Phaser from "phaser";
+import Laser from "./Laser";
+class LaserGroup extends Phaser.Physics.Arcade.Group {
+  constructor(scene) {
+    // Call the super constructor, passing in a world and a scene
+    super(scene.physics.world, scene);
+
+    // Initialize the group
+    this.createMultiple({
+      classType: Laser, // This is the class we create just below
+      // the below value constrains the maximum number of objects on screen at once
+      // this determines how many bullets the player can fire before having to wait
+      frameQuantity: 4,
+      active: false,
+      visible: false,
+      key: "missile",
+    });
+  }
+
+  fireLaser(x, y, xVel, yVel) {
+    const laser = this.getFirstDead(false);
+    if (laser) {
+      laser.fire(x, y, xVel, yVel);
+    }
+  }
+}
+export default LaserGroup;
