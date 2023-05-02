4841aa6ffeb86779d9a6e26121ecab78c8982277
Reagan Armstrong <rarmst0@gmail.com>
2023-05-01 20:42:59 -0400

    Added global high scores to the database and integrated the required public api specified in the project document

--- backend/apiHelpers/createPlayer.js
+++ backend/apiHelpers/createPlayer.js
@@ -1,10 +1,10 @@
-createPlayer = `INSERT INTO players VALUES (?,?,?,?,?)`;
+createPlayer = `INSERT INTO players VALUES (?,?,?,?,?,?)`;
 // name and level
 module.exports = (db, name) => {
   // initialize new player with their given name and starting unlocked level 1
   return new Promise((resolve, reject) => {
     // set level to level 3 for development
-    db.run(createPlayer, [name.trim(), 3, "0", "0", "0"], (err) => {
+    db.run(createPlayer, [name.trim(), 3, 0, 0, 0, 0], (err) => {
       if (err) {
         console.log(err.message);
         reject(err.message);
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const handleLogin = require("./apiHelpers/handleLogin");
 const getDb = require("./apiHelpers/connectToDatabase");
 const config = require(path.join(__dirname, "..", "webpack.config.js"));
 const compiler = webpack(config);
-let port = 3000;
+const axios = require("axios");
+const getHighScores = require("./apiHelpers/getHighScores");
+
+const port = 3000;
 
 const server = express();
 
@@ -38,6 +41,59 @@ server.get("/login", async (req, res) => {
   res.json(player);
 });
 
+// gets the global high scores
+server.get("/high_scores/global", async (req, res) => {
+  const db = getDb();
+  const scores = await getHighScores(db);
+  db.close((err) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Database Closed!");
+    }
+  });
+  res.statusCode = 200;
+  res.statusMessage = "OK";
+
+  res.json(scores);
+});
+
+// gets the glocal high scores, and sends them to the uri if there are 5, if not responds with a 404
+server.post("/high_scores/global", async (req, res) => {
+  const db = getDb();
+  const scores = await getHighScores(db);
+  const data = {
+    data: [
+      {
+        Group: "D",
+        Title: "Top 5 Scores",
+        ...scores,
+      },
+    ],
+  };
+  db.close((err) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Database Closed!");
+    }
+  });
+
+  if (Object.keys(data["data"][0]).length == 7) {
+    // post the global high scores
+    const response = await axios.post(
+      "https://eope3o6d7z7e2cc.m.pipedream.net",
+      data
+    );
+    res.statusCode = response["status"];
+    res.statusMessage = response["statusText"];
+    res.json(scores);
+  } else {
+    res.statusCode = 404;
+    res.statusMessage = "5 High Scores Not Present";
+    res.send();
+  }
+});
 // rebundle js files wih webpack on nodemon refresh
 compiler.watch(
   {
@@ -46,5 +102,3 @@ compiler.watch(
   },
   (err, stats) => {}
 );
-
-
